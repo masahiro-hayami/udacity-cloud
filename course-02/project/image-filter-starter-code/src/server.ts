f30b3c69f8494016ed600507e91ea09ef25a6e98
Hayami Masahiro <masahiro.hayami@woven-planet.global>
2022-09-05 18:48:56 +0900

    09051848 test

--- course-02/project/image-filter-starter-code/src/server.ts
+++ course-02/project/image-filter-starter-code/src/server.ts
@@ -16,7 +16,7 @@ import {filterImageFromURL, deleteLocalFiles} from './util/util';
   // @TODO1 IMPLEMENT A RESTFUL ENDPOINT
   // GET /filteredimage?image_url={{URL}}
   app.get("/filteredimage",async ( req, res ) => {
-    res.send(req.query.name);
+    let { image_url } = req.query;
   } );
      
   // endpoint to filter an image from a public url.
